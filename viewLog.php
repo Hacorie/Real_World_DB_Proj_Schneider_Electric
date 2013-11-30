cd7394ee0a356a180cbd4e3caab3dcdc1d337551
Nathaniel Perry <hacorie@gmail.com>
2013-11-30 17:59:05 -0600

    Added Basic set up for Security Log page

---
+++ viewLog.php
@@ -0,0 +1,26 @@
+<?php
+	
+	session_start();
+	$title = 'View Log';
+
+?>
+
+<?php include "include/header.php"; ?>  
+<table>
+	<tr>
+		<th> ID </th>
+		<th> User Name </th>
+		<th> Login Date/Time </th>
+		<th> IP Address </th>
+		<th> Machine Name </th>
+	</tr>
+	<tr>
+		<td>UID</td>
+		<td>UName</td>
+		<td>LTime</td>
+		<td>IP</td>
+		<td>MName</td>
+	</tr>
+</table>
+
+<?php include "include/footer.php"; ?>
