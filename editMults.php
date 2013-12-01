d7413f5c66d24a9acc89e1566d66008ec382a19c
Nathan Perry <hacorie@gmail.com>
2013-11-30 18:50:01 -0600

    Auto filled editMults page with crap from DB 	modified:   editMults.php

--- editMults.php
+++ editMults.php
@@ -1,7 +1,34 @@
 <?php
-	
+
+    function dbConnect() {
+        $db = new mysqli("mysql.cs.mtsu.edu", "ncr2g", "donthackmebro", "ncr2g");
+
+        if($db->conect_errno) {
+            echo "Failed to connect to MySQL: " . $db->connect_error;
+            exit(1);
+        }
+
+        return $db;
+    }
+
 	session_start();
 	$title = 'Edit Multipliers';
+
+    $db = dbConnect();
+
+    $res = $db->query('SELECT * from Country');
+
+    $country = Array();
+    while($row = $res->fetch_assoc()) {
+        $country[] = $row;
+    }
+
+    $res = $db->query('SELECT * from Product_Type');
+    $parts = Array();
+    while($row = $res->fetch_assoc()) {
+        $parts[] = $row;
+    }
+
 
 ?>
 
@@ -13,10 +40,13 @@
 				<th> Country </th>
 				<th> Multiplier</th>
 			</tr>
+
+            <?php foreach($country as $item) { ?>
 			<tr>
-				<td> <input type="text" name="country" placeholder="Country Name" required /> </td>
-				<td> <input type="text" name="cmultiplier" placeholder="Multiplier" required /> </td>
+				<td> <input type="text" name="country" placeholder="Country Name" value="<?php echo $item['CName'] ?>" required /> </td>
+				<td> <input type="text" name="cmultiplier" placeholder="Multiplier" value="<?php echo $item['Multiplier'] ?>" required /> </td>
 			</tr>
+            <?php } ?>
 		</table>
 		
 		<li><input type="submit" name="submitCountry" value="Edit Country Multipliers" /></li>
@@ -30,11 +60,13 @@
 				<th> Part </th>
 				<th> Multiplier</th>
 			</tr>
+            <?php foreach($parts as $item) { ?>
 			<tr>
-				<td> <input type="text" name="part" placeholder="Part Name" required /> </td>
-				<td> <input type="text" name="pmultiplier" placeholder="Multiplier" required /> </td>
+				<td> <input type="text" name="part" placeholder="Part Name" value="<?php echo $item['PName'] ?>" required /> </td>
+				<td> <input type="text" name="pmultiplier" placeholder="Multiplier" value="<?php echo $item['Multiplier'] ?>" required /> </td>
 			</tr>
-		</table>
+            <?php } ?>
+        </table>
 		
 		<li><input type="submit" name="submitCountry" value="Edit Country Multipliers" /></li>
 	</ul>
