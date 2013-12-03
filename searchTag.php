5f534719a4c7f0912e36df7724b5f267d8ab1658
Alex Williams <cs.alexwilliams@gmail.com>
2013-12-02 19:37:21 -0600

    Added styling to searchTag.php.
    - Removed four columns from search results.
    - Added advanced search.
    - Added toggle between simple and advanced search.
    - Styled the input area appropriately.

--- searchTag.php
+++ searchTag.php
@@ -50,37 +50,55 @@
 
 <div class="page-header">
 	<h1>Search a Tag</h1>
-
-</div>
-	<div class="large-3">
-		<form action="searchTag.php" method="GET">
-			<input type="text" name="query" id="userSearch" placeholder="Enter a Tag" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> />
-			<input type="checkbox" name="old" value="yes" <?php if (isset($_GET['old'])) { echo 'checked="checked"'; } ?> /> Include Older Revisions?
-			<input type="submit" name="search" value="Search" />
+	<div class="btn-group">
+  		<button type="button" class="btn btn-default" onClick="toggleSimpleSearch();" >Simple</button>
+  		<button type="button" class="btn btn-default" onClick="toggleAdvSearch();">Advanced</button>
 	</div>
+</div>
+	<div class="large-3" id="simpleSearch">
+		<form action="searchTag.php" method="GET">
+			<table class="table table-bordered table-striped" style="width: 25%;">
+				<tr><td><input type="text" name="query" id="userSearch" style="width: 100%;" placeholder="Enter Search Criteria" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td></tr>
+			</table>
+			<input type="checkbox" name="old" style="margin-top: 10px;" value="yes" <?php if (isset($_GET['old'])) { echo 'checked="checked"'; } ?> /> Include Older Revisions?
+			<br />
+			<button class="btn btn-s btn-success" type="submit" name="search" style="margin-top: 10px;">Search</button>
+	</div>
+
+	<div class="large-3" style="display: none;" id="advancedSearch">
+		<form action="searchTag.php" method="GET">
+		<table class="table table-bordered table-striped" style="width: 55%;">
+			<tr><td><strong>Tag Number:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter a Tag No." <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td><td><strong>Notes:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter Notes" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td></tr>
+			<tr><td><strong>Revision:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter a Revision No." <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td><td><strong>Install Cost:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter Install Cost" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td></tr>
+			<tr><td><strong>Date:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter a Date" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td><td><strong>Price Note:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter a Price Note" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td></tr>
+			<tr><td><strong>Description:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter a Description" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td><td><strong>Created By:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter Creator" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td></tr>
+			<tr><td><strong>Sub-Category:</strong></td><td><input type="text" name="query" id="userSearch" style="margin-right: 10px;" placeholder="Enter a Sub-Category" <?php if (isset($_GET['query'])) { echo 'value="'.$_GET['query'].'"'; } ?> /></td></tr>
+			</table>
+			<input type="checkbox" name="old" style="margin-top: 15px;" value="yes" <?php if (isset($_GET['old'])) { echo 'checked="checked"'; } ?> /> Include Older Revisions?
+			<br />
+			<button class="btn btn-s btn-success" type="submit" name="search" style="margin-top: 10px;">Search</button>
+	</div>
+
 	<div class="lead">
 		<div class="alert-box success radius" id="userResult" style="display:none;"></div>
 		<div class="alert-box alert radius" id="userError" style="display:none;"></div>
 	</div>
 	<?php if (!empty($searchResults)) { ?>
+		<hr />
 		<div>
+			<p> <h3>Search Results:</h3> </p>
 			<table class="table table-bordered table-striped" id="tags_table">
 				<thead>
 					<tr>
-						<th id="th_tag">Tag</th>
-						<th id="th_rev">Rev</th>
-						<th id="th_date">Date</th>
-						<th id="th_desc">Description</th>
-						<th id="th_subcat">Sub Cat.</th>
-						<th id="th_hvl">HVL</th>
-						<th id="th_cc4">HVL CC4</th>
-						<th id="th_mclad">Metal Clad</th>
-						<th id="th_mvcc"> MV MCC</th>
-						<th id="th_spec">Special Items</th>
-						<th id="th_nodes">Notes</th>
-						<th id="th_install">Install Cost</th>
-						<th id="th_pricenote">Price Note</th>
-						<th id="th_creator">Created By</th>
+						<th id="th_tag" style="text-align: center;">Tag</th>
+						<th id="th_rev" style="text-align: center;">Rev</th>
+						<th id="th_date" style="text-align: center;">Date</th>
+						<th id="th_desc" style="text-align: center;">Description</th>
+						<th id="th_subcat" style="text-align: center;">Sub Cat.</th>
+						<th id="th_nodes" style="text-align: center;">Notes</th>
+						<th id="th_install" style="text-align: center;">Install Cost</th>
+						<th id="th_pricenote" style="text-align: center;">Price Note</th>
+						<th id="th_creator" style="text-align: center;">Created By</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -95,11 +113,6 @@
 						<th><?php echo $tag['CreationDate']; ?></th>
 						<th><?php echo $tag['Description']; ?></th>
 						<th><?php echo $tag['Subcategory']; ?></th>
-						<th></th>
-						<th></th>
-						<th></th>
-						<th></th>
-						<th></th>
 						<th><?php echo $tag['Notes']; ?></th>
 						<th><?php echo $tag['InstallCost']; ?></th>
 						<th><?php echo $tag['PriceNotes']; ?></th>
@@ -112,4 +125,26 @@
 	<?php } ?>
 </div>
 
+<script type="text/javascript"> 
+  
+
+          function toggleAdvSearch() { 
+
+             var simple = document.getElementById('simpleSearch'); 
+             var advanced = document.getElementById('advancedSearch');
+
+		simple.style.display = "none";
+		advanced.style.display = "inline"; 
+          }
+
+          function toggleSimpleSearch() { 
+
+             var simple = document.getElementById('simpleSearch'); 
+             var advanced = document.getElementById('advancedSearch');
+
+		simple.style.display = "inline";
+		advanced.style.display = "none"; 
+          }  
+
+</script> 
 <?php include "include/footer.php"; ?>
